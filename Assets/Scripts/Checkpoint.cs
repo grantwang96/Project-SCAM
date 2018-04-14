b1254fc3fd802b544b393f0cebd4dca54e597fa0
Marcus Guimaraes <mzg218@nyu.edu>
2018-04-13 23:56:03 -0400

    first progress on checkpoints

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Checkpoint : MonoBehaviour {
+
+	Collider col;
+
+	void Start() {
+		col.isTrigger = true;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")) {
+			CheckpointManager.Instance.SaveCheckpoint();
+			gameObject.SetActive(false);
+		}
+	}
+
+}
---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour {
+
+	//singleton
+	static CheckpointManager instance;
+	public static CheckpointManager Instance {
+		get {
+			return instance;
+		}
+	}
+
+
+	string savedDmg = null;
+	string savedMagic = null;
+	Vector3 savedPos;
+	Quaternion savedRot;
+
+	public Transform player;
+	public PlayerDamageable playerDmg;
+	public PlayerMagic playerMagic;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+			instance = this;
+		}
+		else {
+			throw new UnityException("Error: trying to initialize a CheckpointManager while one exists already");
+		}
+
+		SaveCheckpoint();
+	}
+
+	public void SaveCheckpoint() {
+		savedDmg = JsonUtility.ToJson(playerDmg);
+		savedMagic = JsonUtility.ToJson(playerMagic);
+		savedPos = player.position;
+		savedRot = player.rotation;
+	}
+
+	public void ResetToLastCheckpoint() {
+		JsonUtility.FromJsonOverwrite(savedDmg, playerDmg);
+		JsonUtility.FromJsonOverwrite(savedMagic, playerMagic);
+		player.transform.position = savedPos;
+		player.transform.rotation = savedRot;
+	}
+}
